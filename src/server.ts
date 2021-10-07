3cca400dda307958428747e7d173b7814c326452
ueslen santos <ueslencriacao@gmail.com>
2021-10-06 23:12:52 -0300

    config eslint and node and typescript

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express'
 
-const app = express();
+const app = express()
 
 app.get('/', (request, response) => {
-    return response.json({hello: 'olá mundo'})
+  return response.json({ hello: 'olá mundo' })
 })
 
 app.listen(5050)
 
+export class test {
+  teste () {
+    const oi = 'asd'
+  }
+}
