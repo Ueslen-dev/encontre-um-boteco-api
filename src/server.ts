61eb88b12e07484a93abb637fbb84602e5df4a60
ueslen santos <ueslencriacao@gmail.com>
2022-05-03 20:46:01 -0300

    observer dist folder

--- src/server.ts
+++ src/server.ts
@@ -7,13 +7,13 @@ import connectDB from '@config/database'
 
 const app = express()
 const port = process.env.PORT || 5050
-const teste = 'teste'
+
 process.env.PWD = process.cwd()
 
 app.use(cors())
 app.use(express.json())
 app.use(route)
-app.use(express.static('public/uploads'))
+app.use(express.static('dist/uploads'))
 
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
   res.status(error.status || 500)
