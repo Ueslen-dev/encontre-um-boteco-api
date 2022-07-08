42c3440860521b03f693550f7e0c80d583860260
ueslen santos <ueslencriacao@gmail.com>
2022-07-07 23:36:08 -0300

    modifynig model

--- src/models/Pub/index.ts
+++ src/models/Pub/index.ts
@@ -1,15 +1,15 @@
 import { Schema, model } from 'mongoose'
 
 const PubSchema = new Schema({
-  name: { type: String, required: true, maxlength: 35 },
+  name: { type: String, required: true, maxlength: 45 },
   email: { type: String, required: true, maxlength: 255 },
   whatsapp: { type: String, maxlength: 15 },
   instagram: { type: String, maxlength: 35 },
   state: { type: Number, required: true },
   city: { type: Number, required: true },
   cep: { type: Number },
-  address: { type: String, maxlength: 35 },
-  reference: { type: String, maxlength: 35 },
+  address: { type: String, maxlength: 45 },
+  reference: { type: String, maxlength: 45 },
   photo: { type: String, maxlength: 255 },
   responsible: { type: String, maxlength: 20 },
   code: { type: String, maxlength: 16, required: true, unique: true }
